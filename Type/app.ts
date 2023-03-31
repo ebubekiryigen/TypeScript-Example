a0aa92a6c2e457f75f96916214f099eb4f124a6b
Ebubekir yigen <ebushww@gmail.com>
2023-03-31 19:27:07 +0300

    type v1

---
+++ Type/app.ts
@@ -0,0 +1,15 @@
+let fullName:string = 'Ebubekir Yigen'
+const ACTIVE: boolean = true
+const Age:number = 23
+const names: string[] = ['kamil', 'ediz', 'ebubekir']
+let user: {
+    name:string,
+    surname:string,
+    age:number
+}
+
+user = {
+    name:'Ebubekir',
+    surname:'Yigen',
+    age:23
+}
